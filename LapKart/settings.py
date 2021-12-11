c9d5c62795a6bda4c4dd604e8a84eceba0c61975
Ameer-Amr <pameeramrp@gmail.com>
2021-12-11 22:00:28 +0530

    update

--- LapKart/settings.py
+++ LapKart/settings.py
@@ -104,7 +104,8 @@ DATABASES = {
        'NAME':  config('DB_NAME'),
        'USER': config('DB_USER'),
        'PASSWORD': config('DB_PASSWORD'),
-
+       'HOST':config('HOST'),
+       'PORT':config('PORT'),
 
    }
 }
